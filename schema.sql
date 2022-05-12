de93afb7bbd0053026dd326621d570bd88a54052
Victoria <torii383@gmail.com>
2022-05-11 22:13:33 -0500

    schema practice

---
+++ schema.sql
@@ -0,0 +1,31 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE Departments (dept_no VARCHAR(4) NOT NULL, 
+						  dept_name VARCHAR(40) NOT NULL,
+						  Primary key (dept_no), Unique(dept_name));
+Create Table Employees (emp_no Integer Not Null, 
+					   birth_date Date Not Null,
+					   first_name Varchar Not Null,
+					   last_name Varchar Not Null,
+					   gender Varchar Not null,
+					   hire_date Date Not Null,
+					   Primary Key (emp_no)
+					  );
+Create Table Dept_manager(
+	dept_no Varchar(4) Not Null,
+	emp_no INT Not null,
+	from_date Date not null,
+	to_date Date not null,
+Foreign Key (emp_no) References employees(emp_no),
+Foreign Key (dept_no) References departments (dept_no),
+	Primary Key (emp_no, dept_no)
+	);
+Create Table Salaries (
+	emp_no Int Not Null,
+	salary Int Not null,
+	from_date Date not null,
+	to_date Date not null,
+	Foreign Key (emp_no) References employees (emp_no),
+	Primary Key (emp_no)
+	
+);		
+Select * from departments;
