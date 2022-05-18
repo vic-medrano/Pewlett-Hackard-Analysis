fe4818df794565ee247d0361fd5921c3325f95a3
Victoria <torii383@gmail.com>
2022-05-18 18:32:11 -0500

    deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,3 +1,4 @@
+--DELIVERABLE 1
 --Select emp numb, full name, from and to dates to create a table
 SELECT employees.emp_no,
 	   employees.first_name,
@@ -24,6 +25,19 @@ INTO retiring_titles
 from unique_titles
 GROUP BY unique_titles.title
 
-
-
-ORDER BY
+-- DELIVERABLE 2
+Select distinct on (employees.emp_no) employees.emp_no, 
+employees.first_name, 
+employees.last_name, 
+employees.birth_date,
+dept_emp.from_date, 
+dept_emp.to_date,
+titles.title
+INTO mentors
+From employees
+Left outer join dept_emp
+on employees.emp_no = dept_emp.emp_no
+Left outer join titles
+on dept_emp.emp_no = titles.emp_no
+where (dept_emp.to_date = '9999-01-01') AND (employees.birth_date between '1965-01-01' AND '1965-12-31')
+ORder by emp_no ASC
